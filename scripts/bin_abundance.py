b731610c9465cad7e6e30112b5ddda491bc1366c
Val De Anda <val@Vals-MBP.stx.rr.com>
2020-07-15 12:40:08 -0500

    script to compute bin abundance based on metagenomic coverage

---
+++ scripts/bin_abundance.py
@@ -0,0 +1,79 @@
+
+#!/usr/bin/env python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# ------------------------------
+# Name:     Bin_abundance.py
+# Purpose:  calculate the abundance of bins based of scaffold depth
+#
+# @uthors:      vda - valdeanda@utexas.edu
+#
+# Created:     2020
+# Licence:     GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007
+# ------------------------------
+
+
+import argparse
+import pandas as pd
+
+
+# options
+epilog = """Example:
+
+$ python3 bin_abundance.py  -l lenghtpersample.tab  -m scaffolds2sample.tab  -t Taxonomy.tsv"""
+
+parser = argparse.ArgumentParser(description=__doc__, epilog=epilog)
+parser.add_argument('-l', '--length', required=True,
+                    help=("Total lenght of the samples. Rows are samples "
+                    "column is length ."))
+
+parser.add_argument('-m', '--mappingfile', required=True,
+                    help=("Tabular file containing  original contig name "
+		   "Bin, GC, length and Depth in columns"))
+
+
+parser.add_argument('-t', '--taxonomy', required=True,
+                    help=("Tabular file with Bins taxonomy "
+                   "Bin, Proposed_Name, Taxonomy, Superphylum and Confirmed Taxonomy "
+		   "are columns"))
+
+
+
+args = parser.parse_args()
+
+
+length       = args.length
+mapping_file = args.mappingfile
+taxonomy     = args.taxonomy
+
+df_length   = pd.read_csv(length,sep="\t")
+df_mapping  = pd.read_csv(mapping_file,sep="\t")
+df_taxonomy = pd.read_csv(taxonomy, sep ="\t")
+
+
+# Map the total lengt in the mapping file
+
+df2=pd.merge(df_mapping,df_length[['Sample', 'total_length(bp)']], on='Sample')
+
+# Calculate coverage
+
+df2['Cov']= df2['Length'] * df2['Depth']/df2['Length']
+
+# Calculate abundance
+df2['Abund']=df2['Cov']/df2['total_length(bp)']
+#add taxonomy to the mapping file
+#df3=pd.merge(df2,df_taxonomy[['Bin', 'Proposed_Name','Taxonomy','Superphylum','Confirmed Taxonomy']], on='Bin')
+df3=pd.merge(df2,df_taxonomy[['Bin', 'Domain','Class','Proposed taxonomy','Superphylum/Phylum','Notes']], on='Bin')
+#abundance of each bin
+
+df3['Abun_bin'] = df3['Abund'].groupby(df3['Bin']).transform('sum')
+
+outtaxonomy = args.taxonomy + "_abundance.tsv"
+df_final=df3.set_index('Original_Contig_Name')
+df3.to_csv(outtaxonomy,sep='\t')
+
+
+print("[END] Done computing abundance....................\n"
+      "Please check the output file:\n",
+      "Mapping File with Bin Abundance:", args.taxonomy + "_abundance.tsv\n")
